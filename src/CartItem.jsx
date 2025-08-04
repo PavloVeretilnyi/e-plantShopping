ded38bdd404c2fa9ac5a9a3c27e7bbb2997fdcae
PavloVeretilnyi <pashtet9112@gmail.com>
2025-08-04 08:51:09 -0400

    fixed bugs

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -31,13 +31,13 @@ const CartItem = ({ onContinueShopping }) => {
   const handleDecrement = (item) => {
    if(item.quantity > 1) {
         dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
-    }else if(item.quantity === 0){
-        dispatch(removeItem(item));
+    }else{
+        dispatch(removeItem(item.name));
     }
   };
 
   const handleRemove = (item) => {
-    dispatch(removeItem(item));
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
